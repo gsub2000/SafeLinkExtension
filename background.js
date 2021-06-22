00dc89d285ae354f391ca0ac8a05ea376ca23277
Gayatri Subramaniam <gayatrs@uci.edu>
2021-06-21 17:36:25 -0700

    Update background.js

--- background.js
+++ background.js
@@ -1,9 +1,5 @@
-console.log("this is the bg.js file");
-
 chrome.runtime.onMessage.addListener(
     function(request, sender, response){
-        console.log("we did it folks");
-        
         $.ajax({
             url: "https://safe-link-heroku.herokuapp.com/",
             type: "POST",
@@ -11,13 +7,11 @@ chrome.runtime.onMessage.addListener(
             success: function(resp){
                 console.log(resp);
                 response(resp)
-
             },
             error: function(er,a,b){
                 console.log("error has occurred");
             }
         });
         return true;
-        // sendResponse(JSON.stringify(temp));
-        // chrome.runtime.sendMessage(null,"hi")
-    });
+    }
+);
